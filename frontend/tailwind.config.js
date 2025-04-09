3f80d0fbef81bf44cab0445068f154415888a882
AmriteshRaj123 <pathakamritesh6@gmail.com>
2025-04-09 09:33:04 +0530

    update file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,51 @@
+import daisyui from "daisyui";
+import colors from "tailwindcss/colors"; // 👈 required for adding gray
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.gray, // 👈 this brings back text-gray-900, etc.
+      },
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+  },
+};
